4a08f06b0fc682210389c133aa012cfae2687c86
zhangcn <zhaoyu017424@163.com>
2019-12-11 19:49:51 +0800

    这是测试git项目第一次提交  v1.0

---
+++ src/main/java/com/baizhi/Test170GitApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test170GitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test170GitApplication.class, args);
+    }
+
+}
